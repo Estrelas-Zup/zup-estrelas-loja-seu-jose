47f2f052a7f85419984eee27fa9b8f92b84e4130
joaoalveszup <joao.alves@zup.com.br>
2020-11-05 16:52:54 -0300

    Adiciona testes unitários.

--- src/main/java/br/com/zup/estrelas/lojapecas/dto/MensagemDTO.java
+++ src/main/java/br/com/zup/estrelas/lojapecas/dto/MensagemDTO.java
@@ -12,4 +12,21 @@ public class MensagemDTO {
         return mensagem;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MensagemDTO other = (MensagemDTO) obj;
+        if (mensagem == null) {
+            if (other.mensagem != null)
+                return false;
+        } else if (!mensagem.equals(other.mensagem))
+            return false;
+        return true;
+    }
+
 }
--- src/test/java/br/com/zup/estrelas/lojapecas/service/VendaServiceTests.java
+++ src/test/java/br/com/zup/estrelas/lojapecas/service/VendaServiceTests.java
@@ -1,24 +1,93 @@
 package br.com.zup.estrelas.lojapecas.service;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import br.com.zup.estrelas.lojapecas.dao.VendaDAO;
+import br.com.zup.estrelas.lojapecas.dto.MensagemDTO;
+import br.com.zup.estrelas.lojapecas.dto.VendaDTO;
+import br.com.zup.estrelas.lojapecas.entity.Peca;
+import br.com.zup.estrelas.lojapecas.enums.Categoria;
+import br.com.zup.estrelas.lojapecas.repository.VendaRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VendaServiceTests {
-    
-    @Mock
-    VendaDAO vendaDao;
 
     @Mock
     PecaService pecaService;
-    
+
+    @Mock
+    VendaRepository vendaRepository;
 
     @InjectMocks
     VendaService vendaService;
-    
-    
+
+    @Test
+    public void deveRealizarUmaVenda() {
+        Peca peca = new Peca();
+        peca.setCodBarras(111L);
+        peca.setFabricante("VW");
+        peca.setModelo("Polo");
+        peca.setNome("Volante");
+        peca.setQtdEstoque(10);
+        peca.setPrecoCusto(100.5);
+        peca.setPrecoVenda(500.3);
+        peca.setCategoria(Categoria.ACESSORIOS);
+
+        VendaDTO vendaDto = new VendaDTO();
+        vendaDto.setCodBarras(111L);
+        vendaDto.setQuantidade(10);
+
+        Mockito.when(pecaService.buscaPeca(111L)).thenReturn(peca);
+        Mockito.when(pecaService.alteraPeca(Mockito.any(), Mockito.any()))
+                .thenReturn(new MensagemDTO("Peça alterada com sucesso."));
+
+        MensagemDTO mensagemRetornada = this.vendaService.realizaVenda(vendaDto);
+        MensagemDTO mensagemEsperada = new MensagemDTO("Venda realizada com sucesso.");
+
+        Assert.assertEquals("A mensagem retornada deve ser indicativo de sucesso", mensagemEsperada, mensagemRetornada);
+    }
+
+    @Test
+    public void naoDeveRealizarUmaVendaCasoAPecaNaoExista() {
+        VendaDTO vendaDto = new VendaDTO();
+        vendaDto.setCodBarras(111L);
+        vendaDto.setQuantidade(10);
+
+        Mockito.when(pecaService.buscaPeca(111L)).thenReturn(null);
+
+        MensagemDTO mensagemRetornada = this.vendaService.realizaVenda(vendaDto);
+        MensagemDTO mensagemEsperada = new MensagemDTO("Peça inexistente");
+
+        Assert.assertEquals("A mensagem deve indicar peça inexistente", mensagemEsperada, mensagemRetornada);
+    }
+
+    @Test
+    public void naoDeveRealizarUmaVendaCasoNaoHajaPecasSuficientes() {
+        Peca peca = new Peca();
+        peca.setCodBarras(111L);
+        peca.setFabricante("VW");
+        peca.setModelo("Polo");
+        peca.setNome("Volante");
+        peca.setQtdEstoque(10);
+        peca.setPrecoCusto(100.5);
+        peca.setPrecoVenda(500.3);
+        peca.setCategoria(Categoria.ACESSORIOS);
+
+        VendaDTO vendaDto = new VendaDTO();
+        vendaDto.setCodBarras(111L);
+        vendaDto.setQuantidade(15);
+
+        Mockito.when(pecaService.buscaPeca(111L)).thenReturn(peca);
+
+        MensagemDTO mensagemRetornada = vendaService.realizaVenda(vendaDto);
+        MensagemDTO mensagemEsperada = new MensagemDTO("Quantidade inválida");
+
+        Assert.assertEquals("Não deve realizar uma venda quando não há peças suficientes", mensagemEsperada,
+                mensagemRetornada);
+    }
 }
